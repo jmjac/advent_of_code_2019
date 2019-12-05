6a0fd9d0a05856fee2835f9ea0f893d0c6511af8
jmjackowiak <jakub.m.jackowiak@gmail.com>
2019-12-04 23:39:00 -0600

    Combine part1 and part2 into one function

--- day4/main.py
+++ day4/main.py
@@ -1,5 +1,8 @@
-def part1(low: int, top: int) -> int:
-    count = 0
+from typing import Dict
+
+
+def get_combinations(low: int, top: int) -> Dict[int, int]:
+    combinations = []
     for password in range(low, top):
         password = str(password)
         chars = {password[0]: 1}
@@ -12,32 +15,16 @@ def part1(low: int, top: int) -> int:
             if password[j] < password[j - 1]:
                 break
         else:
-            if any(i >= 2 for i in chars.values()):
-                count += 1
-    return count
-
-
-def part2(low: int, top: int) -> int:
-    count = 0
-    for password in range(low, top):
-        password = str(password)
-        chars = {password[0]: 1}
-
-        for j in range(1, 6):
-            if password[j] in chars:
-                chars[password[j]] += 1
-            else:
-                chars[password[j]] = 1
-            if password[j] < password[j - 1]:
-                break
-        else:
-            if 2 in chars.values():
-                count += 1
-    return count
+            combinations.append(chars)
+    return combinations
 
 
 low = 156218
 top = 652527
 
-print(part1(low, top))
-print(part2(low, top))
+combinations = get_combinations(low, top)
+part1 = sum([any(i >= 2 for i in comb.values()) for comb in get_combinations(low, top)])
+part2 = sum([any(i == 2 for i in comb.values()) for comb in get_combinations(low, top)])
+
+print(f"Part1: {part1}")
+print(f"Part2: {part2}")
